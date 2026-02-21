12b5be5f1bed7be2f4231438843c3293d1681fcd
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-21 23:00:10 +0600

    add button toggling function

--- script.js
+++ script.js
@@ -14,13 +14,19 @@ const allCardSection = document.getElementById('all-card');
 const mainContainer = document.querySelector('main');
 const filterSection = document.getElementById('filtered-section');
 
+const jobCount = document.getElementById('job-count');
+jobCount.innerText = allCardSection.children.length;
+
 //  delete card
 const deleteButtons = document.querySelectorAll('.delete-btn');
 deleteButtons.forEach(function(button){
     button.addEventListener('click', function(){
         button.parentElement.parentElement.remove();
+        
+        jobCount.innerText = allCardSection.children.length;
     })
 })
+
 
 // calculate card 
 function calculateCount(){
@@ -29,5 +35,36 @@ function calculateCount(){
     rejectedCount.innerText = rejected.length;
 }
 calculateCount();
+
+function toggleStyle(id){
+    allFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+    interviewFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+    rejectedFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+
+    allFilterBtn.classList.add('bg-[#FFFFFF]', 'text-black');
+    interviewFilterBtn.classList.add('bg-[#FFFFFF]', 'text-black');
+    rejectedFilterBtn.classList.add('bg-[#FFFFFF]', 'text-black');
+
+    const selected = document.getElementById(id);
+    currentStatus = id;
+    selected.classList.remove('bg-[#FFFFFF]', 'text-black');
+    selected.classList.add('bg-[#3B82F6]', 'text-white');
+    
+    if(id === "interview-filter-btn"){
+        allCardSection.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        jobCount.innerText = interview.length;
+    }
+    else if (id === "all-filter-btn"){
+        allCardSection.classList.remove("hidden");
+        filterSection.classList.add("hidden");
+        jobCount.innerText = allCardSection.children.length;
+    }
+    else if (id === "rejected-filter-btn"){
+        allCardSection.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        jobCount.innerText = rejected.length;
+    }
+}
 
 
