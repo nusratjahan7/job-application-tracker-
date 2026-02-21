dd8900baeb6a6e1d2d42531ebae2fd12dcdc6f06
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-21 22:06:00 +0600

    style as figma and connect with js

---
+++ script.js
@@ -0,0 +1,8 @@
+let interview = [];
+let rejected = [];
+let currentStatus = 'all';
+
+let total = document.getElementById('total');
+let interviewCount = document.getElementById('interview-count');
+let rejectedCount = document.getElementById('rejected-count');
+
