7e652ebc5072a21cbdc10dbf0f5d4fbce57804f1
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-21 22:32:45 +0600

    add js for delete card

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ let total = document.getElementById('total');
 let interviewCount = document.getElementById('interview-count');
 let rejectedCount = document.getElementById('rejected-count');
 
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
+const allCardSection = document.getElementById('all-card');
+const mainContainer = document.querySelector('main');
+const filterSection = document.getElementById('filtered-section');
+
+//  delete card
+const deleteButtons = document.querySelectorAll('.delete-btn');
+deleteButtons.forEach(function(button){
+    button.addEventListener('click', function(){
+        button.parentElement.parentElement.remove();
+    })
+})
